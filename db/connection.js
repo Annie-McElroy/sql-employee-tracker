fa539bf4ba05a89713b1aace28e35ca3f2a2eea3
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-08-03 17:48:08 -0500

    Added env file, package.json, and began connection.js to connect to DB through mysql

---
+++ db/connection.js
@@ -0,0 +1,21 @@
+// Import mysql
+const mysql = require('mysql2')
+require('dotenv').config();
+
+// Connection to SQL
+const db = mysql.createConnection(
+    // Database to connect (hidden by dotenv)
+    process.env.DB_NAME,
+    // MySQL username (hidden by dotenv)
+    process.env.DB_USER,
+    // MySQL password (hidden by dotenv)
+    process.env.DB_PASSWORD,
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306,
+    },
+    console.log(`Connected to the employee database.`)
+);
+
+module.exports = db;
